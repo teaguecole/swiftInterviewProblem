8188d03b4594db76a66d2537501543a0bde488a0
Teague Cole <tnc0419@icloud.com>
2022-03-14 23:23:59 -0400

    smallest difference problem

--- Contents.swift
+++ Contents.swift
@@ -1,2 +1,5 @@
 
 InterviewProblems.threeNumberSum(array: [12, 3, 1, 2, -6, 5, -8, 6], targetSum: 0)
+var arrayOne = [-1, 5, 10, 20, 28, 3]
+var arrayTwo = [26, 134, 135, 15, 17]
+InterviewProblems.smallestDifference(arrayA: &arrayOne, arrayB: &arrayTwo)
--- Sources/InterviewProblems.swift
+++ Sources/InterviewProblems.swift
@@ -2,6 +2,8 @@ import Foundation
 
 public class InterviewProblems {
     
+    // Time Complexity O(N^2)
+    // Space Complexity O(N)
     public static func threeNumberSum(array: [Int], targetSum: Int) -> [[Int]] {
         let sorted = array.sorted()
         var triplets = [[Int]]()
@@ -24,7 +26,52 @@ public class InterviewProblems {
         return triplets
     }
     
+    // Time Complexity O(Nlog(N) + Mlog(M))
+    // Space Complexity O(1)
     
+    // Parameters passed in to swift functions are constant, inout allows them to mutated
+    // within the function.  When you call the function, you need make a variable myVar, and
+    // pass it in like &myVar
+    public static func smallestDifference(arrayA: inout [Int], arrayB: inout [Int]) -> [Int] {
+        arrayA.sort()
+        arrayB.sort()
+        
+        var indexA = 0
+        var indexB = 0
+        
+        var smallestDifference = Int.max
+        var currentDifference = Int.max
+        
+        var smallestPair = [Int]()
+        
+        while indexA < arrayA.count && indexB < arrayB.count {
+
+            // This is important! If you dont store firstNum and secondNum,
+            // when you store smallestPair, one of numbers will be
+            // off by an index since idxOne or idxTwo will have changed.
+            // In other words, you can not use arrayOne[idxOne], and
+            // arrayTwo[idxTwo] because those values change from the time
+            // you compare to the time you update smallestPair or return
+            let firstNum = arrayA[indexA]
+            let secondNum = arrayB[indexB]
+            
+            if firstNum < secondNum {
+                currentDifference = secondNum - firstNum
+                indexA = indexA + 1
+            } else if firstNum > secondNum {
+                currentDifference =  firstNum - secondNum
+                indexB = indexB + 1
+            } else {
+                return [firstNum, secondNum]
+            }
+            
+            if smallestDifference > currentDifference {
+                smallestDifference = currentDifference
+                smallestPair = [firstNum, secondNum]
+            }
+        }
+        return smallestPair
+    }
 }
 
 
